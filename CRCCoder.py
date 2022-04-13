835e5a1149adc63dd1cafd41d7ab036e7dfae65c
mprzenzak <mprzenzak@it-dev.pl>
2022-04-13 09:31:48 +0200

    TODO comments added

--- CRCCoder.py
+++ CRCCoder.py
@@ -7,7 +7,11 @@ class CRCCoder:
     polynomial8 = [1, 0, 0, 0, 0, 0, 1, 1, 1]
     polynomial16 = [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
     polynomial32 = [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1]
-
+    # sprawdzic dla wielomianu x+1
+    # ciagi bitow dluzzsze od sumy kontrolnej
+    # opwinno byc tak, ze generuje gdzie jest blad, na ktorym bicie. Zamieniam w liscie zer na tych miejscach na jedynki i robie xora z cala wiadomoscia
+    # requestowanie powtorzen
+    # szum reszty
     crcList = []
     crcList4 = [0, 0, 0, 0]
     crcList8 = [0, 0, 0, 0, 0, 0, 0, 0]
